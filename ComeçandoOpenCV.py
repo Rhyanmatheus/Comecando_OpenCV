d43e597588a2c849478e69594712e2f5f8ed6c13
rhyanmatheus <rhyanmatheus127@gmail.com.com>
2024-10-15 20:37:16 -0300

    lendo um video

--- ComeçandoOpenCV.py
+++ ComeçandoOpenCV.py
@@ -1,8 +1,24 @@
+from tabnanny import check
+
 import cv2
 
-img = cv2.imread('farol.jpg')
-imgCinza = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
+video = cv2.VideoCapture('runners.mp4')
+
+while True:
+    check,img = video.read()
+    cv2.imshow('video',img)
+    cv2.waitKey(1)
+
+
+
+
+
+
+
+#img = cv2.imread('farol.jpg')
+#imgCinza = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
 #print(img.shape)
-cv2.imshow('imagem', img)
-cv2.imshow('imagem CINZA', imgCinza)
-cv2.waitKey(0)
+#cv2.imshow('imagem', img)
+#cv2.imshow('imagem CINZA', imgCinza)
+#cv2.waitKey(0)
+
