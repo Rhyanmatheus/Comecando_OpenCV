c8566b37fddabde3b3c6e3d9cd4cc33a5820cc67
rhyanmatheus <rhyanmatheus127@gmail.com.com>
2024-10-15 21:16:39 -0300

    lendo um video

--- ComeçandoOpenCV.py
+++ ComeçandoOpenCV.py
@@ -2,12 +2,33 @@ from tabnanny import check
 
 import cv2
 
-video = cv2.VideoCapture('runners.mp4')
+camera = cv2.VideoCapture(0)
+camera.set(3,640) #largura
+camera.set(4,420) #altura
+camera.set(10,100) # Brilho/luminosidade
 
 while True:
-    check,img = video.read()
-    cv2.imshow('video',img)
-    cv2.waitKey(1)
+    check,img = camera.read()
+    cv2.imshow('webcam',img)
+    if cv2.waitKey(1) & 0xFF ==ord('q'):
+        break
+
+
+
+
+
+
+
+
+
+
+#video = cv2.VideoCapture('runners.mp4')
+
+#while True:
+    #check,img = video.read()
+    #imgRedim = cv2.resize(img,(640,420))
+    #cv2.imshow('video',imgRedim)
+    #cv2.waitKey(10)
 
 
 
